ca832349578e8d1511ffb95c15490550030bbdef
maksim.shediakov <shedyakovm@gmail.com>
2023-06-19 09:39:44 +0300

    404

--- wp-app/wp-content/themes/gkb-theme/404.php
+++ wp-app/wp-content/themes/gkb-theme/404.php
@@ -1,8 +1,11 @@
 <?php get_header();
+$image = get_field('image_404', 'option')
 ?>
-<section>
-    <div class="container">
-
-    </div>
-</section>
+    <section>
+        <div class="container">
+            <?php if (!empty($image)) : ?>
+                <img src="<?= $image['url'] ?>" alt="<?= $image['title'] ?>">
+            <?php endif; ?>
+        </div>
+    </section>
 <?php get_footer(); ?>
