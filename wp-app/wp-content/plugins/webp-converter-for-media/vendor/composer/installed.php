99f5d195e9649d06cbd7c465d1829c51eb19dfc6
denis.shediakov <luckyswd@gmail.com>
2023-06-24 16:03:37 +0300

    add Converter for Media

---
+++ wp-app/wp-content/plugins/webp-converter-for-media/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gbiorczyk/webp-converter-for-media',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd683d65879c4c7503c489bad0d3868ba76b11e15',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'gbiorczyk/webp-converter-for-media' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd683d65879c4c7503c489bad0d3868ba76b11e15',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'matt-plugins/deactivation-modal' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => 'dc52aa8c8793d1c11eb973c0f77b63f8abc2d9bf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../matt-plugins/deactivation-modal',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
